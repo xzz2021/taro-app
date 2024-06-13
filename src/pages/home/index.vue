05e3292ec403f94a8b7453a4d997b86e1e35cfe1
xzz2021 <zipeng.2016@gmail.com>
2024-06-13 15:58:24 +0800

    添加轮播图swiper

---
+++ src/pages/home/index.vue
@@ -0,0 +1,16 @@
+<template>
+   <view> 
+     <xzzswiper />
+     <xzzcontainer />
+   </view>
+</template>
+
+<script setup>
+import xzzswiper from './swiper.vue'
+import xzzcontainer from './container.vue'
+
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/home/swiper.vue
@@ -0,0 +1,35 @@
+<template>
+    <nut-swiper
+     :init-page="2"
+     :auto-play="3000"
+     pagination-visible
+     pagination-color="#426543"
+     pagination-unselected-color="#808080"
+   >
+     <nut-swiper-item v-for="(item, index) in list" :key="index" style="height: 250px">
+       <img :src="item" alt="" style="height: 100%; width: 100%" draggable="false" />
+     </nut-swiper-item>
+   </nut-swiper>
+
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { Swiper, SwiperItem } from '@nutui/nutui-taro'
+const list = ref([
+  'https://yun3d.com/filestore/assets/app/home1.jpg',
+  'https://yun3d.com/filestore/assets/app/home1.jpg',
+  'https://yun3d.com/filestore/assets/app/home1.jpg',
+])
+
+</script>
+
+<style>
+.demo {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
